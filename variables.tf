4a514bef630d3a9874e5bf87e7b98f4c95ad39c9
Sreelal <sreelalr@gmail.com>
2022-01-23 21:00:47 +0530

    added variables

--- variables.tf
+++ variables.tf
@@ -1,1 +1,32 @@
-variable "AWS_
+variable "AWS_SECRET_KEY" {}
+
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "Security_Group" {
+  type = list
+  default = ["sg-tf1", "sg-tf2", "sg-tf3"]
+}
+
+variable "AMIS" {
+  type = map
+  default = {
+      us-east-1 = "ami-00fa576fb10a52a1c"
+      us-east-2 = "ami-005cfd0b0e6a6a2ea"
+      us-west-1 = "ami-066c6938fb715719f"
+      us-west-2 = "ami-09eb8e9e19fe359fc"
+  }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "~/aws-keys/levelup_key"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "~/aws-keys/levelup_key.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+    default = "ubuntu"
+}
